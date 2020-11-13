31b480b5dcbe218913fb50a04062ae4d85ac3797
i2kiselev <griggs1299@gmail.com>
2020-11-13 19:07:27 +0800

    Initial commit

---
+++ src/main/java/ru/isu/i2kiselev/courseeditor/CourseEditorApplication.java
@@ -0,0 +1,13 @@
+package ru.isu.i2kiselev.courseeditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseEditorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/isu/i2kiselev/courseeditor/CourseEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.isu.i2kiselev.courseeditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseEditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
